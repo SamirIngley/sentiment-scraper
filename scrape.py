5ca2fe57ce11bdb40f3fc2a03b9bfd521eb6ba74
SamirIngley <samir.ingle7@gmail.com>
2019-10-17 11:04:39 -0700

    html in terminal

--- scrape.py
+++ scrape.py
@@ -1,7 +1,24 @@
 from bs4 import BeautifulSoup
 import requests
 
-with open('samplesite.html') as html_file:
-    soup = BeautifulSoup(html_file, 'lxml')
+# with open('samplesite.html') as html_file:
+#     soup = BeautifulSoup(html_file, 'lxml')
+#
+# # match = soup.title.text # gives text of first title tag
+# # print(soup.prettify()) # indents html file more readable
+# # print(match)
+#
+# article = soup.find('div', class_='article') # div tag w footer class
+# # print(article)
+#
+# headline = article.h2.a.text # dig into headline - h2 tag, a tag, only text
+# print(headline)
+#
+# summary = article.p.text # p tag text gives summary here
+# print(summary)
 
-print(soup.prettify())
+
+source = requests.get('https://medium.com').text
+soup = BeautifulSoup(source, 'lxml')
+article = soup.find('article')
+print(article.prettify())
