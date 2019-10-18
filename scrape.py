436170d5b7a8fb3cba1c99c527df4ba4d5473a43
SamirIngley <samir.ingle7@gmail.com>
2019-10-17 19:54:22 -0700

    medium scrape complete

--- scrape.py
+++ scrape.py
@@ -1,23 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
 
-# with open('samplesite.html') as html_file:
-#     soup = BeautifulSoup(html_file, 'lxml')
-#
-# # match = soup.title.text # gives text of first title tag
-# # print(soup.prettify()) # indents html file more readable
-# # print(match)
-#
-# article = soup.find('div', class_='article') # div tag w footer class
-# # print(article)
-#
-# headline = article.h2.a.text # dig into headline - h2 tag, a tag, only text
-# print(headline)
-#
-# summary = article.p.text # p tag text gives summary here
-# print(summary)
-
-
 source = requests.get('https://medium.com/topic/editors-picks').content
 soup = BeautifulSoup(source, 'lxml')
 #print(soup.prettify())
@@ -25,27 +8,14 @@ soup = BeautifulSoup(source, 'lxml')
 # with open('index2.html', 'w') as out:
 #     out.write(soup.prettify())
 
-article_list = soup.select('h3') # list of articles
-for article in article_list:
-    print(article.text)
-#print(article_list.text)
+articles = soup.find_all('section', class_="gt gu ae co l gv gw gx gy gz")
 
-# for article in article_list:
-#     print(article.title)
-#     print(article.)
-#     print(article.text)
-#     # try:
-#     headline = article.find_all('h3').text
-#     # except Exception as e:
-#     #     headline = article.h2.text
-#
-#     # author = article.find('a', class_="ds-link ds-link--styleSubtle postMetaInline postMetaInline--author").text
-#     # time = article.find('span', class_="readingTime")['title']
-#     # summary = article.find('div', class_="ui-summary ui-clamp3").text
-#     # link = article.find()
-#
-#     print(headline)
-#     # print('By: ' + author + '   ' + time)
-#     # print(summary)
-#
-#     print()
+for article in articles:
+    print()
+    headline = article.find('h3', class_="am ax ej bt ek bu fo hi hj ap as fp ep eq ar")
+    print(headline.text)
+    author = article.find('span', class_="bt b bu bv bw bx ap as es ep eq ar am ax")
+    time = article.find('div', class_="gg t cv")
+    print('     ' + author.text + '       ' + time.text)
+    summary = article.find('h3', class_="bt fm fn fo as fp ep ap eq ar by")
+    print(summary.text)
